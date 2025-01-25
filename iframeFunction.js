dd4e3b30e610fc501c3c75142d42aca096dba0d3
HotflixBD <119289128+HotflixBD@users.noreply.github.com>
2025-01-25 18:01:59 +0900

    Update iframeFunction.js

--- iframeFunction.js
+++ iframeFunction.js
@@ -62,3 +62,20 @@ function openCity(evt, cityName) {
   // display the match date time
   document.getElementById("output").innerText = outputFinal;
 }
+
+
+
+
+var i1 = document.querySelector('#t1'); 
+var l1='https://streambtw.com/iframe/ch6.php';
+var l='https://www.warnoir.online/p/media-player.html?embed=https://v5.sportsonline.si/channels/hd/hd2.php';
+var l='https://me.webcric.com/frame5.htm';
+i1.setAttribute('src', l1); 
+
+var i2 = document.querySelector('#t2'); 
+var l2='https://streamtp2.com/global1.php?stream=dazn_laliga';
+i2.setAttribute('src', l2); 
+
+var i3 = document.querySelector('#t3'); 
+var l3='https://la12hd.com/vivo/canal.php?stream=m_laligatv';
+i3.setAttribute('src', l3); 
