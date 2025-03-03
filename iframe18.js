da60590cb3a3e9c68dad0c8787403386cf176aa7
HotflixBD <119289128+HotflixBD@users.noreply.github.com>
2025-03-03 14:44:39 +0900

    Update iframe18.js

--- iframe18.js
+++ iframe18.js
@@ -1,3 +1,3 @@
     var i1 = document.querySelector('#t11'); 
-    var l1='https://www.yosintv.net/iframe.html?url=https://tinyurl.com/28hsgn9n';
+    var l1='https://tinyurl.com/hls-player/iframe.html?url=https://tinyurl.com/28hsgn9n';
     i1.setAttribute('src', l1); 
