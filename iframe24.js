6ebfaeec8b48b00868ba80170e73e250d4d3b4fa
HotflixBD <119289128+HotflixBD@users.noreply.github.com>
2025-03-03 14:58:39 +0900

    Update iframe24.js

--- iframe24.js
+++ iframe24.js
@@ -1,7 +1,7 @@
     var i1 = document.querySelector('#t1'); 
-    var l1='https://tinyurl.com/hls-player/iframe.html?url=https://tinyurl.com/24fbbyc6';
+    var l1='https://tinyurl.com/hls-player/iframe.html?url=https://me.webcric.com/frame3.htm';
     i1.setAttribute('src', l1); 
 
     var i2 = document.querySelector('#t2'); 
-    var l2='https://tinyurl.com/hls-player/iframe.html?url=https://tinyurl.com/2b49gqkm';
+    var l2='https://tinyurl.com/hls-player/iframe.html?url=https://me.webcric.com/willow.htm';
     i2.setAttribute('src', l2); 
