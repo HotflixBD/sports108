53ce7282762d6f50451e315a0b7bc0b36517d0b4
HotflixBD <119289128+HotflixBD@users.noreply.github.com>
2025-03-03 14:44:26 +0900

    Update iframe17.js

--- iframe17.js
+++ iframe17.js
@@ -1,3 +1,3 @@
     var i1 = document.querySelector('#t11'); 
-    var l1='https://tinyurl.com/2bnoupqh';
+    var l1='https://tinyurl.com/hls-player/iframe.html?url=https://tinyurl.com/28hsgn9n';
     i1.setAttribute('src', l1); 
